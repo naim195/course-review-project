204c40d04f07ca7e2d88978c8b2d9d0afa232678
naim195 <119221365+naim195@users.noreply.github.com>
2024-07-07 10:18:54 +0530

    testing auth

--- client/src/AuthContext.jsx
+++ client/src/AuthContext.jsx
@@ -1,8 +1,8 @@
-import  { createContext, useState, useEffect } from 'react';
-import axios from 'axios';
-import { IconButton, Snackbar, SnackbarContent } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-import PropTypes from 'prop-types';
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+import { IconButton, Snackbar, SnackbarContent } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import PropTypes from "prop-types";
 
 export const AuthContext = createContext();
 
@@ -10,22 +10,11 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [snackbarOpen, setSnackbarOpen] = useState(false);
-  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarMessage, setSnackbarMessage] = useState("");
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   useEffect(() => {
     checkAuth();
-  }, []);
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const userDataParam = urlParams.get('userData');
-    if (userDataParam) {
-      const userData = JSON.parse(decodeURIComponent(userDataParam));
-      setUser(userData);
-      window.history.replaceState({}, document.title, window.location.pathname);
-      showSnackbar('Signed in successfully!');
-    }
   }, []);
 
   const checkAuth = async () => {
@@ -37,7 +26,7 @@ export const AuthProvider = ({ children }) => {
         setUser(response.data.user);
       }
     } catch (error) {
-      console.error('Auth check failed', error);
+      console.error("Auth check failed", error);
     } finally {
       setLoading(false);
     }
@@ -47,36 +36,28 @@ export const AuthProvider = ({ children }) => {
     try {
       const newWindow = window.open(
         `${backendUrl}/auth/google`,
-        '_blank',
-        'width=500,height=600',
+        "_blank",
+        "width=500,height=600",
       );
-
-      await new Promise((resolve, reject) => {
+      const { data } = await new Promise((resolve, reject) => {
         const interval = setInterval(() => {
           if (newWindow.closed) {
             clearInterval(interval);
-            reject(new Error('Google sign-in process was canceled'));
-          }
-
-          try {
-            const currentUrl = newWindow.location.href;
-            if (currentUrl.startsWith('https://course-review-project-phi.vercel.app/')) {
-              clearInterval(interval);
-              const urlParams = new URLSearchParams(new URL(currentUrl).search);
-              const userData = JSON.parse(decodeURIComponent(urlParams.get('userData')));
-              setUser(userData);
-              resolve();
-              newWindow.close();
-            }
-          } catch (e) {
-            // Ignore errors caused by cross-origin restrictions
+            reject(new Error("Google sign-in process was canceled"));
           }
         }, 500);
+        window.addEventListener("message", (event) => {
+          console.log(event.origin);
+          if (event.origin === backendUrl && event.data && event.data.user) {
+            clearInterval(interval);
+            resolve({ data: event.data });
+          }
+        });
       });
-
-      showSnackbar('Signed in successfully!');
+      setUser(data.user);
+      showSnackbar("Signed in successfully!");
     } catch (error) {
-      console.error('Google sign-in failed:', error);
+      console.error("Google sign-in failed:", error);
     }
   };
 
@@ -85,16 +66,15 @@ export const AuthProvider = ({ children }) => {
       const response = await axios.get(`${backendUrl}/auth/logout`, {
         withCredentials: true,
       });
-
       if (response.status === 200) {
         setUser(null);
-        showSnackbar('Logged out successfully!');
+        showSnackbar("Logged out successfully!");
       } else {
-        showSnackbar('Logout failed!');
+        showSnackbar("Logout failed!");
       }
     } catch (error) {
-      showSnackbar('An error occurred during logout.');
-      console.error('An error occurred during logout:', error);
+      showSnackbar("An error occurred during logout.");
+      console.error("An error occurred during logout:", error);
     }
   };
 
@@ -105,7 +85,7 @@ export const AuthProvider = ({ children }) => {
 
   const handleCloseSnackbar = () => {
     setSnackbarOpen(false);
-    setSnackbarMessage('');
+    setSnackbarMessage("");
   };
 
   return (
@@ -115,24 +95,24 @@ export const AuthProvider = ({ children }) => {
       {children}
       <Snackbar
         anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
+          vertical: "bottom",
+          horizontal: "left",
         }}
         open={snackbarOpen}
         autoHideDuration={6000}
         onClose={handleCloseSnackbar}
       >
         <SnackbarContent
-          sx={{ backgroundColor: '#4caf50' }}
+          sx={{ backgroundColor: "#4caf50" }}
           message={snackbarMessage}
           action={
             <IconButton
-              size='small'
-              aria-label='close'
-              color='inherit'
+              size="small"
+              aria-label="close"
+              color="inherit"
               onClick={handleCloseSnackbar}
             >
-              <CloseIcon fontSize='small' />
+              <CloseIcon fontSize="small" />
             </IconButton>
           }
         />
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,29 +1,26 @@
-const express = require('express');
-const passport = require('passport');
+const express = require("express");
+const passport = require("passport");
 
 const router = express.Router();
 
-const successLoginUrl = 'https://course-review-project-phi.vercel.app/auth-success';
-const failureLoginUrl = 'https://course-review-project-phi.vercel.app/login/error';
+// const successLoginUrl = process.env.FRONTEND_URL + '/auth-success';
+const failureLoginUrl = 'https://course-review-project-phi.vercel.app//login/error';
 
-router.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] })
-);
+router.get("/auth/google", passport.authenticate("google", { scope: ["email", "profile"] }));
 
-router.get(
-  '/google/callback',
-  passport.authenticate('google', {
-    failureRedirect: failureLoginUrl,
-    failureFlash: 'Something went wrong',
-  }),
-  (req, res) => {
-    const userDataParam = encodeURIComponent(JSON.stringify(req.user));
-    res.redirect(`${successLoginUrl}?userData=${userDataParam}`);
-  }
-);
+router.get("/google/callback", passport.authenticate("google", {
+  failureRedirect: failureLoginUrl,
+  failureFlash: "Something went wrong",
+}), (req, res) => {
+  res.send(`
+    <script>
+      window.opener.postMessage({ user: ${JSON.stringify(req.user)} }, '*');
+      window.close();
+    </script>
+  `);
+});
 
-router.get('/auth/check', (req, res) => {
+router.get("/auth/check", (req, res) => {
   if (req.isAuthenticated()) {
     res.status(200).json({ user: req.user });
   } else {
@@ -31,7 +28,7 @@ router.get('/auth/check', (req, res) => {
   }
 });
 
-router.get('/auth/logout', (req, res, next) => {
+router.get("/auth/logout", (req, res, next) => {
   req.logout((err) => {
     if (err) {
       return next(err);
@@ -40,7 +37,7 @@ router.get('/auth/logout', (req, res, next) => {
       if (err) {
         return next(err);
       }
-      res.status(200).json({ message: 'Logged out successfully!' });
+      res.status(200).json({ message: "Logged out successfully!" });
     });
   });
 });
